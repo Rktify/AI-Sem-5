2b2ff089151c17be0cec73352cc138ef4f1f77aa
Wilbert <91464358+Rktify@users.noreply.github.com>
2023-10-23 09:38:55 +0700

    Create Triangle.pl

---
+++ Forum_Week6/Triangle.pl
@@ -0,0 +1,24 @@
+is_triangle(Angle1, Angle2, Angle3) :-
+    Angle1 > 0, Angle2 > 0, Angle3 > 0,
+    Total is Angle1 + Angle2 + Angle3,
+    Total =:= 180.
+
+is_right_triangle(Angle1, Angle2, Angle3) :-
+    member(90, [Angle1, Angle2, Angle3]).
+
+main :-
+    write('Enter the three angles of the triangle: '),
+    read(Angle1),
+    read(Angle2),
+    read(Angle3),
+    
+    (is_triangle(Angle1, Angle2, Angle3) ->
+        write('These angles form a valid triangle.'),
+        (is_right_triangle(Angle1, Angle2, Angle3) ->
+            writeln(' It is also a right triangle.');
+            writeln(' It is not a right triangle.')
+        );
+        write('These angles do not form a valid triangle.')
+    ).
+
+:- initialization(main).
